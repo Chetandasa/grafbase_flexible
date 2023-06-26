5f2f9d2b873040c9ca0a93da4000656fa45a4dda
Your Name <chetandasa2814@gmail.com>
2023-06-26 15:42:51 +0530

    Deploying  now

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -15,7 +15,7 @@ const ProfilePage = ({ user }: Props) => (
             <div className='flex items-start flex-col w-full'>
                 <Image src={user?.avatarUrl} width={100} height={100} className="rounded-full" alt="user image" />
                 <p className="text-4xl font-bold mt-10">{user?.name}</p>
-                <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">I’m Software Engineer at JSM 👋</p>
+                <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">I’m Full Stack Developer 👋</p>
                 
                 <div className="flex mt-8 gap-5 w-full flex-wrap">
                     <Button 
